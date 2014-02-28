eead7ffc0b8d3f0d6988844b59fb0662ebc97fe6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-03-01 03:31:37 +1200

    + java.lang.System out

---
+++ lang/System/out.java
@@ -0,0 +1,11 @@
+public class out {
+
+  public static void main(String[] argv) {
+
+    java.io.PrintStream ps = System.out;
+
+    ps.println("System.out returns a java.io.PrintStream");
+
+  }
+
+}
