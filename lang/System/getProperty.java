bf47ea7edbce0c293ccca35c9158017a31ca637e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-03-01 03:23:25 +1200

    + java.lang.System getProperty()

---
+++ lang/System/getProperty.java
@@ -0,0 +1,28 @@
+public class getProperty {
+
+  static private void  p(String propertyName) {
+
+    System.out.println(String.format("%1$-" + 20 + "s", propertyName + ":") + System.getProperty(propertyName));
+
+  }
+
+  public static void main(String[] argv) {
+
+    p("file.separator"  ); //   "/" on UNIX and "\" on Windows.
+    p("java.class.path" ); //   Try   java -cp foo:bar:. getProperty   to see effect
+    p("java.home"       ); //   Installation directory for JRE
+    p("java.vendor"     ); //   JRE vendor name
+    p("java.vendor.url" ); //   JRE vendor URL
+    p("java.version"    ); //   JRE version number
+    p("line.separator"  ); //   How text files seperate lines
+    p("os.arch"         ); //   Processor?
+    p("os.name"         );
+    p("os.version"      );
+    p("path.separator"  ); //   : on Unix and ; on Windows?
+    p("user.dir"        ); //   Working directory
+    p("user.home"       ); //   Home directory
+    p("user.name"       );
+
+  }
+
+}
