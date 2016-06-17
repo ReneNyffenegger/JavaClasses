c4ba46e9d5809f3aaf3182fb0440e3bc4d0362fc
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-06-17 19:47:23 +0200

    java.lang.String valueOf()

---
+++ java/lang/String/valueOf.java
@@ -0,0 +1,30 @@
+public class valueOf {
+
+  public static void main(String[] argv) {
+
+    int i = 42;
+    String s = String.valueOf(i);
+    System.out.println(s);
+
+    alternatives();
+  }
+
+  private static void alternatives() {
+    alternative_1();
+    alternative_2();
+  }
+
+  private static void alternative_1() {
+    int i = 38;
+
+    String s = Integer.toString(i);
+    System.out.println(s);
+  }
+
+  private static void alternative_2() {
+    int i = -17;
+    String s = "" + i;
+    System.out.println(s);
+  }
+
+}
