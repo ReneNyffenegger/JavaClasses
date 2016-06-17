912addf369fb3f57f6a298b492ac2325eb594338
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-06-17 19:36:29 +0200

    + java.util.Random nextInt()

---
+++ java/util/Random/nextInt.java
@@ -0,0 +1,16 @@
+public class nextInt {
+
+  public static void main(String[] argv) {
+
+    java.util.Random r = new java.util.Random();
+
+    for (int i=0; i<50; i++) {
+
+      System.out.println(r.nextInt(100));
+
+    }
+
+
+  }
+
+}
