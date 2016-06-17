1b1cc58edc9b8e7d05811ec750900133ce56e4d1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-06-17 19:45:55 +0200

    add some alternatives

--- java/lang/Integer/toString.java
+++ java/lang/Integer/toString.java
@@ -3,10 +3,26 @@ public class toString {
   public static void main(String[] argv) {
 
     int i = 42;
-
     String s = Integer.toString(42);
-
     System.out.println(s);
 
+    alternatives();
+  }
+
+  private static void alternatives() {
+    alternative_1();
+    alternative_2();
+  }
+
+  private static void alternative_1() {
+    int i = 38;
+    String s = String.valueOf(i);
+    System.out.println(s);
+  }
+
+  private static void alternative_2() {
+    int i = -17;
+    String s = "" + i;
+    System.out.println(s);
   }
 }
