6fd0521cc575223fd63a50f5e2719eda4753b06c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-11-03 07:37:29 +0100

    java.lang.System: println

---
+++ java/lang/System/println.java
@@ -0,0 +1,9 @@
+public class println {
+
+  public static void main(String[] args) {
+    java.lang.System.out.println("one"  );
+    java.lang.System.out.println("two"  );
+    java.lang.System.out.println("three");
+  }
+
+}
